d69c9ebee9dda01cf09eabe24655db5af7db18b5
ThaVip <musasidiabubakar@gmail.com>
2024-02-24 13:39:07 -0800

    its akkf

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,36 +1,25 @@
 #include "sort.h"
-
 /**
- * bubble_sort - sorts an array of integers in ascending order
- * using the Bubble sort algorithm
- * @array: Array to sort
+ * bubble_sort - sorts an array of integers in ascending order using
+ * the Bubble sort algorithm
+ * @array: pointer to array
  * @size: size of the array
- *
- * Return: Void
- */
+ **/
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, size2 = size;
-	int tmp, flag = 0;
+	size_t i, j, n;
 
-	if (array == NULL || size < 2)
-		return;
 	for (i = 0; i < size; i++)
 	{
-		for (j = 1; j < size2; j++)
+		for (j = 1; j < size - i; j++)
 		{
 			if (array[j - 1] > array[j])
 			{
-				flag = 1;
-				tmp = array[j];
+				n = array[j];
 				array[j] = array[j - 1];
-				array[j - 1] = tmp;
+				array[j - 1] = n;
 				print_array(array, size);
 			}
 		}
-		if (flag == 0)
-			break;
-		flag = 0;
-		size2--;
 	}
 }
